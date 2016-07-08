feecb9830190ad3e931e1f5c01b8d93c00744227
Shradha Pulla <pullashradha@gmail.com>
2016-07-08 08:39:08 -0600

    Create bool method IsWordInArray() to find if wordInput is in sentenceArray, need to make sentenceArray outside of CountRepeats() method

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -7,16 +7,16 @@ namespace WordMatches.Objects
 {
   public class RepeatCounter
   {
-    // private string _wordInput;
+    private string _wordInput;
     private string _sentenceInput;
-    // public string GetWord()
-    // {
-    //   return _wordInput;
-    // }
-    // public void SetWord(string newWord)
-    // {
-    //   _wordInput = newWord;
-    // }
+    public string GetWord()
+    {
+      return _wordInput;
+    }
+    public void SetWord(string newWord)
+    {
+      _wordInput = newWord;
+    }
     public string GetSentence()
     {
       return _sentenceInput;
@@ -25,11 +25,22 @@ namespace WordMatches.Objects
     {
       _sentenceInput = newSentence;
     }
+    public bool IsWordInArray()
+    {
+      foreach (string word in _sentenceArray)
+      {
+        if (_wordInput == word)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
     public int CountRepeats()
     {
-      string[] sentenceArray = _sentenceInput.Split();
+      string[] _sentenceArray = _sentenceInput.Split();
       int numberArrayElements = 0;
-      foreach (string word in sentenceArray)
+      foreach (string word in _sentenceArray)
       {
         numberArrayElements += 1;
       }
--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -12,13 +12,13 @@ namespace WordMatches.Objects
       newRepeatCounter.SetSentence("We love dogs");
       Assert.Equal(3, newRepeatCounter.CountRepeats());
     }
-    // [Fact]
-    // public void CountRepeats_IsWordInArray_true()
-    // {
-    //   RepeatCounter newRepeatCounter = new RepeatCounter ();
-    //   newRepeatCounter.SetWord("dogs");
-    //   newRepeatCounter.SetSentence("We love dogs");
-    //   Assert.Equal(true, newRepeatCounter.CountRepeats());
-    // }
+    [Fact]
+    public void CountRepeats_IsWordInArray_true()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter ();
+      newRepeatCounter.SetWord("dogs");
+      newRepeatCounter.SetSentence("We love dogs");
+      Assert.Equal(true, newRepeatCounter.IsWordInArray());
+    }
   }
 }
