c356072f4d5172848af6a6def782851045413608
Shradha Pulla <pullashradha@gmail.com>
2016-07-12 19:11:11 -0600

    Uncomment the tests in RepeatCounterTest.cs

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -5,20 +5,20 @@ namespace WordMatches.Objects
 {
   public class RepeatCounterTest
   {
-    // [Fact]
-    // public void CountRepeats_DisplaySentenceArray_true()
-    // {
-    //   RepeatCounter newRepeatCounter = new RepeatCounter ();
-    //   newRepeatCounter.SetSentence("I love dogs");
-    //   Assert.Equal("I love dogs", newRepeatCounter.CountRepeats());
-    // }
-    // [Fact]
-    // public void CountRepeats_CountElementsInArray_3()
-    // {
-    //   RepeatCounter newRepeatCounter = new RepeatCounter ();
-    //   newRepeatCounter.SetSentence("We love dogs");
-    //   Assert.Equal(3, newRepeatCounter.CountRepeats());
-    // }
+    [Fact]
+    public void CountRepeats_DisplaySentenceArray_true()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter ();
+      newRepeatCounter.SetSentence("I love dogs");
+      Assert.Equal("I love dogs", newRepeatCounter.CountRepeats());
+    }
+    [Fact]
+    public void CountRepeats_CountElementsInArray_3()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter ();
+      newRepeatCounter.SetSentence("We love dogs");
+      Assert.Equal(3, newRepeatCounter.CountRepeats());
+    }
     [Fact]
     public void CountRepeats_WordRepeatedInArray_true()
     {
