e09661ece4265f05d1b493943e77c01edd41d1ee
Shradha Pulla <pullashradha@gmail.com>
2016-07-17 10:35:32 -0600

    Update CountRepeats() method to ignore a char in front of the wordInput, pass 4th test

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -6,7 +6,7 @@ namespace WordMatches.Objects
   public class RepeatCounterTest
   {
     [Fact]
-    public void Test_TimesWordRepeatedInArray_1()
+    public void Test_CountsTimesWordRepeatedInSentence_1()
     {
       RepeatCounter newRepeatCounter = new RepeatCounter();
       newRepeatCounter.SetWord("dogs");
@@ -14,7 +14,7 @@ namespace WordMatches.Objects
       Assert.Equal(1, newRepeatCounter.CountRepeats());
     }
     [Fact]
-    public void Test_ExcludeSimilarValues_true()
+    public void Test_ExcludeSimilarWordValues_true()
     {
       RepeatCounter newRepeatCounter = new RepeatCounter();
       newRepeatCounter.SetWord("dogs");
@@ -26,8 +26,16 @@ namespace WordMatches.Objects
     {
       RepeatCounter newRepeatCounter = new RepeatCounter();
       newRepeatCounter.SetWord("dogs");
-      newRepeatCounter.SetSentence("We love dogs! Do you like dogs?");
-      Assert.Equal(0, newRepeatCounter.CountRepeats());
+      newRepeatCounter.SetSentence("We love dogs! Do you love dogs?");
+      Assert.Equal(2, newRepeatCounter.CountRepeats());
+    }
+    [Fact]
+    public void Test_CountsWordWithCharPreceding_true()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter();
+      newRepeatCounter.SetWord("dogs");
+      newRepeatCounter.SetSentence("What did they name their pet -dogs");
+      Assert.Equal(1, newRepeatCounter.CountRepeats());
     }
   }
 }
